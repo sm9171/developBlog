36f46e0e03a96708d604595e3c72ac34f4b62469
na <sm9171@nate.com>
2019-09-29 23:34:58 +0900

    2222

--- src/components/info.js
+++ src/components/info.js
@@ -14,14 +14,12 @@ export default function info() {
       <div className="my-5"></div>
       <div className="display-3" style={{ color: "#FF8A00" }}>
         <span style = {{color : "white"}}>Hello, I'm</span> <br />
-        Sumanth.
+        Naccoon.
       </div>
       <div className="h1 code mt-4 mb-3">async {"{"}</div>
       <div className="text-muted mx-5 my-4 h3 text-justify info">
         A Passionate Front-end Developer, UI/UX Enthusiast, Android Geek and Web
         Developer.
-        <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et.
       </div>
       <div className="h1 code mt-2 mb-3">{"}"}</div>
       <div className="h1 mt-5">
